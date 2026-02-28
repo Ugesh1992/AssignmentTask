e1708f521198412f19d3cb2046659f504b0c8d52
Ugesh1992 <ugesh.kumargzb@gmail.com>
2026-02-28 10:34:47 +0530

    Update Assignment5.py

--- Assignment5.py
+++ Assignment5.py
@@ -1,3 +1,4 @@
+# Task1
 student = {"Rajesh" : 59, "Ram" : 40, "Deepak" : 83, "Rahul" : 73}
 name = input("Enter the student's Name : ")
 
@@ -5,3 +6,14 @@ if name in student:
     print(f"{name}'s marks are : {student[name]}")
 else:
     print("Student is not found")
+
+
+#Task 2
+
+numlst = list(range(1,11))
+resfive = numlst[:5]
+revlst = resfive[::-1]
+
+print(f"Original List : {numlst}")
+print(f"Extracted first five element : {resfive}")
+print(f"Reversed extracted element : {revlst}")
